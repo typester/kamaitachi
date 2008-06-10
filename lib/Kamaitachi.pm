7eeb0a9a2a37b65981d846a6c9a032011400e7c5
Daisuke Murase <typester@cpan.org>
2008-06-10 23:38:52 +0900

    initial commit

---
+++ lib/Kamaitachi.pm
@@ -0,0 +1,38 @@
+package Kamaitachi;
+use strict;
+use warnings;
+
+our $VERSION = '0.01';
+
+=head1 NAME
+
+Kamaitachi - Module abstract (<= 44 characters) goes here
+
+=head1 SYNOPSIS
+
+  use Kamaitachi;
+  blah blah blah
+
+=head1 DESCRIPTION
+
+Stub documentation for this module was created by ExtUtils::ModuleMaker.
+It looks like the author of the extension was negligent enough
+to leave the stub unedited.
+
+Blah blah blah.
+
+=head1 AUTHOR
+
+Daisuke Murase <typester@cpan.org>
+
+=head1 COPYRIGHT
+
+This program is free software; you can redistribute
+it and/or modify it under the same terms as Perl itself.
+
+The full text of the license can be found in the
+LICENSE file included with this module.
+
+=cut
+
+1;
