e62a25635e4e1126f610b7a9ed9e51d0097c35ac
hidek <hide@hide-k.net>
2008-12-04 14:47:09 +0900

    added StreamAudienceCounter pod

--- lib/Kamaitachi/Service/StreamAudienceCounter.pm
+++ lib/Kamaitachi/Service/StreamAudienceCounter.pm
@@ -42,6 +42,39 @@ before on_close => sub  {
     }
 };
 
-
 1;
 
+__END__
+
+=encoding utf8
+
+=head1 NAME
+
+Kamaitachi::Service::StreamingAudienceCounter - service role to count and broadcast streaming audience
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=head1 METHODS
+
+=head2 get_stream_audience_count
+
+L<Kamaitachi>,
+L<Kamaitachi::Service>
+
+=head1 AUTHOR
+
+Daisuke Murase <typester@cpan.org>
+
+Hideo Kimura <hide@cpan.org>
+
+=head1 COPYRIGHT
+
+This program is free software; you can redistribute
+it and/or modify it under the same terms as Perl itself.
+
+The full text of the license can be found in the
+LICENSE file included with this module.
+
+=cut
